64cfeeb46c9806870cb8c97a6a320f1f2de43fe7
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-03-28 21:38:09 -0300

    refactor: move SUT creation to a factry method

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -7,13 +7,17 @@ import axios from 'axios'
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
 
+const makeSut = (): AxiosHttpClient => {
+  return new AxiosHttpClient()
+}
+
 // A classe de teste do AxiosHttpClient
 describe('AxiosHttpClient', () => {
   // Vamos testar a url. Se a url passada para o axios é a mesma que passamos para o AxiosHttpClient e que la é passada para o axios
   test('Should call axios with correct URL', async () => {
     const url = faker.internet.url()
 
-    const sut = new AxiosHttpClient()
+    const sut = makeSut()
     await sut.post({ url })
     expect(mockedAxios).toHaveBeenCalledWith(url)
   })
