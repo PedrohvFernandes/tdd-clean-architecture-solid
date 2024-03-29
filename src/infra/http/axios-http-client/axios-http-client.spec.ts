83135b9ca8590fad0ebb7f6afcaf58900983cc6c
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-03-28 22:16:20 -0300

    test: ensure AxiosHttpClient return correct values

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { AxiosHttpClient } from './axios-http-client'
+import { AxiosHttpClientAdapter } from './axios-http-client-adapter'
 
 import { HttpPostParams } from '@/data/protocols/http'
 import { faker } from '@faker-js/faker'
@@ -8,9 +8,16 @@ import axios from 'axios'
 // Mockando a lib do axios. Fazendo isso temos acesso a todas as funções do axios
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
+const mockedAxiosResult = {
+  status: faker.number.bigInt,
+  data: faker.helpers.objectEntry({
+    key: faker.string.uuid()
+  })
+}
+mockedAxios.post.mockResolvedValue(mockedAxiosResult)
 
-const makeSut = (): AxiosHttpClient => {
-  return new AxiosHttpClient()
+const makeSut = (): AxiosHttpClientAdapter => {
+  return new AxiosHttpClientAdapter()
 }
 
 const mockPostoRequest = (): HttpPostParams<any> => ({
@@ -20,9 +27,9 @@ const mockPostoRequest = (): HttpPostParams<any> => ({
   })
 })
 
-// A classe de teste do AxiosHttpClient
-describe('AxiosHttpClient', () => {
-  // Vamos testar a url. Se a url passada para o axios é a mesma que passamos para o AxiosHttpClient e que la é passada para o axios e se estou usando o verbo correto que é o post para chamar a url, e testamos se o body passado para o axios é o mesmo que passamos para o AxiosHttpClient
+// A classe de teste do AxiosHttpClientAdapter
+describe('AxiosHttpClientAdapter', () => {
+  // Vamos testar a url. Se a url passada para o axios é a mesma que passamos para o AxiosHttpClientAdapter e que la é passada para o axios e se estou usando o verbo correto que é o post para chamar a url, e testamos se o body passado para o axios é o mesmo que passamos para o AxiosHttpClientAdapter
   test('Should call axios with correct values', async () => {
     const request = mockPostoRequest()
 
@@ -31,11 +38,13 @@ describe('AxiosHttpClient', () => {
     expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
   })
 
-  test('Should return', async () => {
-    const request = mockPostoRequest()
-
+  // O nosso http response responde com um status code e um body. Então vamos testar se o axios retorna o status code e o body correto
+  test('Should return the correct and body', async () => {
     const sut = makeSut()
-    await sut.post(request)
-    expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
+    const httpResponse = await sut.post(mockPostoRequest())
+    expect(httpResponse).toEqual({
+      statusCode: mockedAxiosResult.status,
+      body: mockedAxiosResult.data
+    })
   })
 })
