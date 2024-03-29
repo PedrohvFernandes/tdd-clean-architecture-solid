24cc2203fff35d6adfc9da88ccf17fff36ac068e
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-03-28 22:16:50 -0300

    feat: ensure AxiosHttpClient return correct values

---
+++ src/infra/http/axios-http-client/axios-http-client-adapter.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  HttpPostClient,
+  HttpPostParams,
+  HttpResponse
+} from '@/data/protocols/http'
+import axios from 'axios'
+
+// Dessa forma fazemos com que o axios dependa da minha interface HttpPostClient e o resto da aplicação nao dependa do axios
+export class AxiosHttpClientAdapter implements HttpPostClient<any, any> {
+  // Aqui como pode retornar qualquer coisa do Axios, usamos o any como generico
+  async post(params: HttpPostParams<any>): Promise<HttpResponse<any>> {
+    // await axios(params.url)
+    // O primeiro param é a url da API e o segundo é o body(data) da requisição
+    const httpResponse = await axios.post(params.url, params.body)
+    return {
+      statusCode: httpResponse.status,
+      body: httpResponse.data
+    }
+  }
+}
