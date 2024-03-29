7c72c886c8b7aa51b976cdd849249fb977aca8fe
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-03-28 21:59:26 -0300

    test: ensure AxiosHttpClient calls axios with correct body

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { AxiosHttpClient } from './axios-http-client'
 
+import { HttpPostParams } from '@/data/protocols/http'
 import { faker } from '@faker-js/faker'
 import axios from 'axios'
 
@@ -11,14 +13,29 @@ const makeSut = (): AxiosHttpClient => {
   return new AxiosHttpClient()
 }
 
+const mockPostoRequest = (): HttpPostParams<any> => ({
+  url: faker.internet.url(),
+  body: faker.helpers.objectEntry({
+    key: faker.string.uuid()
+  })
+})
+
 // A classe de teste do AxiosHttpClient
 describe('AxiosHttpClient', () => {
-  // Vamos testar a url. Se a url passada para o axios é a mesma que passamos para o AxiosHttpClient e que la é passada para o axios e se estou usando o verbo correto que é o post para chamar a url
-  test('Should call axios with correct URL and verb post', async () => {
-    const url = faker.internet.url()
+  // Vamos testar a url. Se a url passada para o axios é a mesma que passamos para o AxiosHttpClient e que la é passada para o axios e se estou usando o verbo correto que é o post para chamar a url, e testamos se o body passado para o axios é o mesmo que passamos para o AxiosHttpClient
+  test('Should call axios with correct values', async () => {
+    const request = mockPostoRequest()
 
     const sut = makeSut()
-    await sut.post({ url })
-    expect(mockedAxios.post).toHaveBeenCalledWith(url)
+    await sut.post(request)
+    expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
+  })
+
+  test('Should return', async () => {
+    const request = mockPostoRequest()
+
+    const sut = makeSut()
+    await sut.post(request)
+    expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
   })
 })
