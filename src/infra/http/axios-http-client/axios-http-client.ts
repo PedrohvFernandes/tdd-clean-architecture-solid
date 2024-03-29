ed09a3a029f76f7b87558d90d1743b5e5aaa7b63
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-03-28 21:58:59 -0300

    feat: ensure AxiosHttpClient calls axios with correct body

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -6,6 +6,7 @@ export class AxiosHttpClient {
   // Aqui como pode retornar qualquer coisa do Axios, usamos o any como generico
   async post(params: HttpPostParams<any>): Promise<void> {
     // await axios(params.url)
-    await axios.post(params.url)
+    // O primeiro param é a url da API e o segundo é o body(data) da requisição
+    await axios.post(params.url, params.body)
   }
 }
