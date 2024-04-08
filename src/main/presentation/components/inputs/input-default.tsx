2084903e4c7881cf1c2d19bbb9f77704ad6e9a8c
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-04-08 17:38:45 -0300

    feat: remove autofill input on chrome

--- src/main/presentation/components/inputs/input-default.tsx
+++ src/main/presentation/components/inputs/input-default.tsx
@@ -1,4 +1,4 @@
-import { HTMLAttributes, InputHTMLAttributes } from 'react'
+import React, { HTMLAttributes, InputHTMLAttributes } from 'react'
 
 import { IconBallRed } from '../icon-ball-red'
 
@@ -9,9 +9,16 @@ export function InputDefault(
   props: InputDefaultProps,
   { ...rest }: DivDefaultProps
 ) {
+  // A gente desativa a questão de readOnly para que o usuário possa digitar
+  const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
+    event.target.readOnly = false
+  }
   return (
     <div {...rest} className="flex items-center relative">
       <input
+        // readOnly para desativar a sugestão do google chrome, so que ele impede do usuario digitar
+        readOnly
+        onFocus={enableInput}
         {...props}
         className="border border-primary-LIGHT p-5 rounded focus:outline-primary-LIGHT focus:ring-2 ring-primary-DARK flex-1"
       />
