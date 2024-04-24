011a08c36110ee3ede64e8fffe78eef0286a3500
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-04-24 15:08:38 -0300

    test: ensure RequiredFieldValidation returns falsy if field is not  empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,6 +1,8 @@
 import { RequiredFieldError } from '../errors'
 import { RequiredFieldValidation } from './required-field-validation'
 
+import { faker } from '@faker-js/faker'
+
 describe('RequiredFieldValidation', () => {
   test('Should return error if field is empty', () => {
     // Validamos o campo email
@@ -8,5 +10,13 @@ describe('RequiredFieldValidation', () => {
     // Passamos um campo vazio para o validador de email
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError())
+  })
+
+  test('Should return falsy if field is not empty', () => {
+    const sut = new RequiredFieldValidation('email')
+    // Validamos se contem um valor no campo email
+    const error = sut.validate(faker.internet.email())
+    // Se não retornar erro, então o campo é valido
+    expect(error).toBeFalsy()
   })
 })
