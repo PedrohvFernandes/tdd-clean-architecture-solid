b061dc2d422fdd11fefe420423cff32b2cea449f
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-04-25 13:17:42 -0300

    test: ensure EmailValidation returns error if email is invalid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,3 +1,12 @@
+// Eu tenho tres alternativas para testar o EmailValidation:
+// 1. Testar o email com libs de terceiros na camada de infra, ou seja, montaríamos os testes, iramos colocar um protocolo(interface) de email aqui nessa camada, pois não testaríamos diretamente aqui, mas usaríamos os testes de integração para testar a camada de infra, usando a lib de validação(yup, zod, etc) através das interfaces que criamos. E por se tratar da camada de infra, ela pode ter as libs que forem necessarias para rodarem a aplicação, como por exemplo, o axios, yup, etc
+// 2. Testar o email com libs de terceiros na camada de validação(Pior dos casos, pois a camada de validação ficaria acoplada a lib de validação em si)m, seria o mais facil, mas não é o ideal
+// 3. Testar o email com regex(expressão regular) na camada de validação, ou seja, a validação do email seria feita diretamente aqui, por nós mesmos
+
+// Nesse caso optamos por fazer a 3, por ser algo mais simples de fazer
+
+// Caso no projeto usemos a lib yup, por exemplo, para validar o email, teríamos que usar a primeira opção, porque dessa forma iriamos usar uma lib para validar, mas a camada de validation não ficaria acoplada a lib de validação em si. Mas como não temos a lib yup no projeto, vamos fazer a terceira opção, que é mais simples de fazer
+
 import { EmailValidation } from './email-validation'
 
 import { InvalidFieldError } from '@/validation/errors'
