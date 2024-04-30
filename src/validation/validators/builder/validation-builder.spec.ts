8519cfca6a01cec25780e2667d2a30a5e2bac4fc
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-04-30 16:26:19 -0300

    test: ensure ValidationBuilder return a list of validations

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -5,26 +5,46 @@ import {
   MinLengthValidation,
   RequiredFieldValidation
 } from '@/validation/validators'
+import { faker } from '@faker-js/faker'
 
 describe('ValidationBuilder', () => {
   test('Should return RequiredFieldValidation', () => {
+    const fieldName = faker.database.column()
     // Um campo pode ter mais de uma validação, e para isso eu chamo o metodo field, passando o nome do campo, e depois chamo o metodo required, que é uma validação de campo obrigatório, e no final chamo o metodo build, que vai retornar um array de validações. Apos o required poderia chamar outros metodos para testar outras validações, nesse caso so testamos o required desse campo especifico(Ex: any_field).
-    const validations = sut.field('any_field').required().build()
-    expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+    const validations = sut.field(fieldName).required().build()
+    expect(validations).toEqual([new RequiredFieldValidation(fieldName)])
   })
 
   test('Should return EmailValidation', () => {
-    const validations = sut.field('any_field').email().build()
-    expect(validations).toEqual([new EmailValidation('any_field')])
+    const fieldName = faker.database.column()
+
+    const validations = sut.field(fieldName).email().build()
+    expect(validations).toEqual([new EmailValidation(fieldName)])
   })
 
   test('Should return MinLengthValidation', () => {
-    const validations = sut.field('any_field').min(5).build()
-    expect(validations).toEqual([new MinLengthValidation('any_field', 5)])
+    const fieldName = faker.database.column()
+    const length = Number(faker.string.numeric())
+
+    const validations = sut.field(fieldName).min(length).build()
+    expect(validations).toEqual([new MinLengthValidation(fieldName, length)])
   })
 
-  test('Should return MinLengthValidation', () => {
-    const validations = sut.field('any_field').min(5).build()
-    expect(validations).toEqual([new MinLengthValidation('any_field', 5)])
+  // Teste para validar se o metodo build esta retornando uma lista de validações, e não apenas uma validação para aquele campo(ex: any_field) que esta sendo testado.
+  test('Should return a list of validations', () => {
+    const fieldName = faker.database.column()
+    const length = Number(faker.string.numeric())
+
+    const validations = sut
+      .field(fieldName)
+      .required()
+      .min(length)
+      .email()
+      .build()
+    expect(validations).toEqual([
+      new RequiredFieldValidation(fieldName),
+      new MinLengthValidation(fieldName, length),
+      new EmailValidation(fieldName)
+    ])
   })
 })
