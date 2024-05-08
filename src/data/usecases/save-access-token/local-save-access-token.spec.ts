dffbbf8ee9f8271d80df69b229501a57d4150146
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-05-08 13:01:43 -0300

    test: ensure LocalSaveAccessToken throws if SetStorage throws

--- src/data/usecases/save-access-token/local-save-access-token.spec.ts
+++ src/data/usecases/save-access-token/local-save-access-token.spec.ts
@@ -28,5 +28,15 @@ describe('LocalSaveAccessToken', () => {
     expect(setStorageMock.value).toBe(accessToken)
   })
 
-  // É um teste meio desnecerário, pois o teste anterior já cobre tudo o que precisamos. No caso esse teste aqui é uma execeção, hoje o localstorage que é o que iremos usar para salvar o token do usuário não retorna nada, mas caso um dia usarmos algo, uma lib que retorne algo, esse teste seria util.
+  // É um teste meio desencerarão, pois o teste anterior já cobre tudo o que precisamos. No caso esse teste aqui é uma exceção, hoje o localstorage que é o que iremos usar para salvar o token do usuário não retorna nada, mas caso um dia usarmos algo, uma lib que retorne algo, esse teste seria util.
+  // Esse teste garante que se der um erro no nosso caso uso não queremos que tenha um try catch, queremos que o erro seja propagado para quem chamou o método, no caso o componentes do React, e ai ele decide o que ele faz com o erro
+  test('Should throw uf SetStorage throws', async () => {
+    const { sut, setStorageMock } = makeSut()
+
+    jest.spyOn(setStorageMock, 'set').mockRejectedValueOnce(new Error())
+
+    const promise = sut.save(faker.string.uuid())
+
+    await expect(promise).rejects.toThrow(new Error())
+  })
 })
