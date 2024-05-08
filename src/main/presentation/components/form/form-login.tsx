0f7045b6cd567dca94147d8b61c55f498de3dd33
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-05-08 12:20:05 -0300

    feat: ensure LocalSaveAccessToken calls SetStorage with correct value

--- src/main/presentation/components/form/form-login.tsx
+++ src/main/presentation/components/form/form-login.tsx
@@ -32,7 +32,7 @@ export function FormLogin({
 
       setIsLoading()
       const account = await authentication.auth({ email, password })
-      // O localstorage não pode ficar aqui, ele faz parte do infra e vira um novo caso de uso no domain, porque se futuramente mudarmos por exemplo para cookies a maneira de salvar os dados isso não fica nada reutilizavel, ou seja, eu teria que mudar em todo componente que tem ele. Isso seria um novo caso de uso porque ele é uma regra de negocio, onde eu salvo o token de acesso
+      // O localstorage não pode ficar aqui, ele faz parte do infra e vira um novo caso de uso no domain, implementado no data e dentro do infra usamos a lib(ex: localStorage...) para armazenar localmente, para não acoplar o projeto com libs externas(como foi feito para o axios remote-authentication) porque se futuramente mudarmos por exemplo para cookies a maneira de salvar os dados isso não fica nada reutilizavel, ou seja, eu teria que mudar em todo componente que tem ele. Isso seria um novo caso de uso porque ele é uma regra de negocio, onde eu salvo o token de acesso localmente.
       localStorage.setItem('accessToken', account.accessToken)
       navigate('/')
     } catch (error) {
