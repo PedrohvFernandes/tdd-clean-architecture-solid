917722ac789690fd692dcabc28d25e55db6bf155
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-05-08 13:03:01 -0300

    test: ensure LocalSaveAccessToken throws if SetStorage throws

--- src/data/test/mock-storage.ts
+++ src/data/test/mock-storage.ts
@@ -1,11 +1,14 @@
 import { SetStorage } from '../protocols/cache'
 
 /*
-  Existe tres tipos de mocks:
+  Existe tres tipos de mocks/dublês de testes, são eles:
 
   O Spy --> O spy é um mock que captura valores de parâmetros passados e retorna um valor mockado.
+
   O Stub --> Onde não capturamos os valores de parâmetros, apenas retornamos um valor fixo. O professor fez isso em validation --> ValidationStub na camada de presentation. No meu caso eu consegui validar o valor do parâmetro(Que era se os campos estavam certos), então não usei stub, e sim Spy --> ValidationSpy
-  O Mock --> Captura os valores e não precisa mocar nada
+
+  O Mock --> Captura os valores e não precisa mocar nada, retornar nada
+
 */
 export class SetStorageMock implements SetStorage {
   value: any
@@ -14,6 +17,7 @@ export class SetStorageMock implements SetStorage {
   set(key: string, value: any): Promise<void> {
     this.key = key
     this.value = value
+    // Nesse caso eu retorno uma promise vazia, pois não temos nada para retornar
     return Promise.resolve()
   }
 }
