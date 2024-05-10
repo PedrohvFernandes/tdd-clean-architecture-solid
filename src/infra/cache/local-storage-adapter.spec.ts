4d2608582b806e9f145ad0e213866df95945bb01
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-05-09 21:41:20 -0300

    test: ensure LocalStorageAdapter calls localStorage with correct values

--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -13,6 +13,7 @@ describe('LocalStorageAdapter', () => {
     localStorage.clear()
   })
 
+  // Não testamos exceções aqui porque o localStorage ñ possui exceções, so testamos se o localStorage foi chamado com os valores corretos. E aqui de fato na infra é a implementação do localStorage, no data é a interface, la da para testar exceções porque ele não sabe qual lib vai ser usada para implementar a interface, logo futuramente podemos usar outra lib para implementar a interface e nele talvez tenha exceções
   test('Should call localStorage with correct values', async () => {
     const sut = makeSut()
     const key = faker.database.column()
