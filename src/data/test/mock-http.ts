5f14295dd50df33f191e7621e4a83859e2b7eaf9
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-05-13 14:27:43 -0300

    refactor: rename data mocks

--- src/data/test/mock-http.ts
+++ src/data/test/mock-http.ts
@@ -1,15 +1,24 @@
-/*
-  Dentro da pasta do test vamos colocar todas as versões spy que são tipo as implementações da camada de infra para data usando a interface do data layer, logo as spy são consideradas como um mock, são as versões mocadas Spy da verdadeira implementação na camada de infra, porque quase todos os casos de uso vão utilizar httpClient --> Post, get... então nada melhor que separar e se tornar algo único para reaproveitar esse mock em vários lugares
-
-  Aqui vamos colocar todos os mocks que tiverem haver com httpclient, não precisa ser so o post, pode ser o get, put, delete, patch, etc...
-*/
-
 import {
   HttpPostClient,
   HttpPostParams,
   HttpResponse,
   HttpStatusCode
 } from '@/data/protocols/http'
+import { faker } from '@faker-js/faker'
+
+/*
+  Dentro da pasta do test vamos colocar todas as versões spy que são tipo as implementações da camada de infra para data usando a interface do data layer, logo as spy são consideradas como um mock, são as versões mocadas Spy da verdadeira implementação na camada de infra, porque quase todos os casos de uso vão utilizar httpClient --> Post, get... então nada melhor que separar e se tornar algo único para reaproveitar esse mock em vários lugares
+
+  Aqui vamos colocar todos os mocks que tiverem haver com httpclient, não precisa ser so o post, pode ser o get, put, delete, patch, etc...
+*/
+
+// Mock para requisição para testar na parte de infra
+const mockPostRequest = (): HttpPostParams<any> => ({
+  url: faker.internet.url(),
+  body: faker.helpers.objectEntry({
+    key: faker.string.uuid()
+  })
+})
 
 // Spy é um duble de teste, é um tipo de mock, que serve para duas coisas:
 // Spy é um objeto que vai espionar o método, e não vai fazer nada, só vai verificar se o método foi chamado com o valor correto
@@ -39,4 +48,4 @@ class HttpPostClientSpy<T, R> implements HttpPostClient<T, R> {
   }
 }
 
-export { HttpPostClientSpy }
+export { HttpPostClientSpy, mockPostRequest }
