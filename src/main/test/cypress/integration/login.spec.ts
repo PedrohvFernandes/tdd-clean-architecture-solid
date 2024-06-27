2513bdd612e812667a0ec9481d6f141cec533c2a
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-06-27 19:39:35 -0300

    test: ensure Login present error message if invalid credential aare provided

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -2,6 +2,8 @@
 
 import { faker } from '@faker-js/faker'
 
+const baseUrl = Cypress.config().baseUrl
+
 describe('Login', () => {
   // Como sempre iremos fazer isso antes de cada teste, podemos usar o beforeEach para fazer isso.
   beforeEach(() => {
@@ -77,5 +79,30 @@ describe('Login', () => {
     cy.getByTestId('submit').should('not.have.attr', 'disabled')
 
     cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present error if invalid credentials are provider', () => {
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.string.alphanumeric(5))
+
+    cy.getByTestId('submit').click()
+
+    // Estou procurando um elemento dentro de outro elemento. Primeiro eu entro no pai e depois procuro o filho(s). Eu olho se o ellipsis esta aparecendo e se o main-error não esta aparecendo e depois verifico se o ellipsis não esta aparecendo e o main-error esta aparecendo.
+    cy.getByTestId('error-wrap')
+      .getByTestId('ellipsis')
+      .should('exist')
+      .getByTestId('main-error')
+      .should('not.exist')
+      .getByTestId('ellipsis')
+      .should('not.exist')
+      .getByTestId('main-error')
+      .should('exist')
+    // .should(
+    //   'contain.text',
+    //   'Algo de errado aconteceu. Tente novamente em breve.'
+    // )
+
+    // Eq(equal) é uma função do cypress que verifica se a url é igual a que passamos para ela.
+    cy.url().should('eq', `${baseUrl}/login`)
   })
 })
