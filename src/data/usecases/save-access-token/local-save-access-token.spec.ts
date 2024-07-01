cd6246640c193540d61910abfad7e64f06330e6d
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-07-01 16:50:31 -0300

    test: ensure LocalSaveAccesToken throws if no accessToken is provided

--- src/data/usecases/save-access-token/local-save-access-token.spec.ts
+++ src/data/usecases/save-access-token/local-save-access-token.spec.ts
@@ -1,6 +1,7 @@
 import { LocalSaveAccessToken } from './local-save-access-token'
 
 import { SetStorageMock } from '@/data/test'
+import { UnexpectedError } from '@/domain/errors'
 import { faker } from '@faker-js/faker'
 
 type SutTypes = {
@@ -30,7 +31,7 @@ describe('LocalSaveAccessToken', () => {
 
   // É um teste meio desencerarão, pois o teste anterior já cobre tudo o que precisamos. No caso esse teste aqui é uma exceção, hoje o localstorage que é o que iremos usar para salvar o token do usuário não retorna nada, mas caso um dia usarmos algo, uma lib que retorne algo, esse teste seria util.
   // Esse teste garante que se der um erro no nosso caso uso não queremos que tenha um try catch, queremos que o erro seja propagado para quem chamou o método, no caso o componentes do React, e ai ele decide o que ele faz com o erro
-  test('Should throw uf SetStorage throws', async () => {
+  test('Should throw if SetStorage throws', async () => {
     const { sut, setStorageMock } = makeSut()
 
     jest.spyOn(setStorageMock, 'set').mockRejectedValueOnce(new Error())
@@ -38,5 +39,13 @@ describe('LocalSaveAccessToken', () => {
     const promise = sut.save(faker.string.uuid())
 
     await expect(promise).rejects.toThrow(new Error())
+  })
+
+  test('Should throw if accessToken if falsy', async () => {
+    const { sut } = makeSut()
+
+    const promise = sut.save(undefined as any)
+
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
