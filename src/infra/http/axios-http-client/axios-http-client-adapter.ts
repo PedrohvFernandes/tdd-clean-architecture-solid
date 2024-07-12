4e265da25007e7facd883d2825b9fbe8d7080aba
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-07-12 18:06:31 -0300

    feat: ensure AxiosHttpClient calls axios.ghet with correct values

--- src/infra/http/axios-http-client/axios-http-client-adapter.ts
+++ src/infra/http/axios-http-client/axios-http-client-adapter.ts
@@ -1,4 +1,5 @@
 import {
+  HttpGetParams,
   HttpPostClient,
   HttpPostParams,
   HttpResponse
@@ -44,5 +45,9 @@ export class AxiosHttpClientAdapter implements HttpPostClient {
       statusCode: axiosResponse.status,
       body: axiosResponse.data
     }
+  }
+
+  async get(params: HttpGetParams): Promise<void> {
+    await axios.get(params.url)
   }
 }
