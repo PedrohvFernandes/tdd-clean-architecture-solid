fa41c4c0bd5b4ccb8ff1089543a8688775b1e991
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-07-31 18:25:30 -0300

    feat: ensure LocalStorageAdapter calls localStorage.getIrem with correct value

--- src/infra/cache/local-storage-adapter.ts
+++ src/infra/cache/local-storage-adapter.ts
@@ -1,7 +1,13 @@
-import { SetStorage } from '@/data/protocols/cache'
+import { GetStorage, SetStorage } from '@/data/protocols/cache'
 
-export class LocalStorageAdapter implements SetStorage {
+export class LocalStorageAdapter implements SetStorage, GetStorage {
   set(key: string, value: object): void {
     localStorage.setItem(key, JSON.stringify(value))
+  }
+
+  get(key: string): any {
+    // Nem sempre é o account, podemos usar esse mesmo metodo para achar outra coisa. O mesmo vale para o set, para set qualquer chave:valor
+    const objAny = JSON.parse(localStorage.getItem(key) as string)
+    return objAny
   }
 }
