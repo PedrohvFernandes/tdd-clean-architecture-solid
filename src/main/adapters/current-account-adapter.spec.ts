67b75b3ea0ec4d9a3a2df3146d9db3b89777d42f
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-07-31 17:23:57 -0300

    test: ensure CurrentAccountAdapter throws if an invalid account is provided

--- src/main/adapters/current-account-adapter.spec.ts
+++ src/main/adapters/current-account-adapter.spec.ts
@@ -1,5 +1,6 @@
 import { setCurrentAccountAdapter } from './current-account-adapter'
 
+import { UnexpectedError } from '@/domain/errors'
 import { mockAccountModel } from '@/domain/test'
 import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter'
 
@@ -17,5 +18,14 @@ describe('CurrentAccountAdapter', () => {
 
     // Esperamos que o método set seja chamado com a chave 'account' e o valor da conta -->  set(key: string, value: object): void
     expect(setSpy).toHaveBeenCalledWith('account', account)
+  })
+
+  test('Should throw UnexpectedError', () => {
+    // Quando não passamos a conta ou o token da conta, ele deve lançar um erro
+    // É dessa forma que testamos exceções, passando uma função que lança a exceção e esperando que ela seja lançada.
+    // Geralmente quando fazemos esse tipo de teste a gente pega uma dependencia da funcão e mocka a dependencia para retornar um valor que faça a função lançar a exceção. Mas nesse caso como é a própria função que lança a exceção, não precisamos fazer isso.
+    expect(() => {
+      setCurrentAccountAdapter(undefined as any)
+    }).toThrow(new UnexpectedError())
   })
 })
