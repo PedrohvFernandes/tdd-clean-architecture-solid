eae912a31e87f28ad67ec3eea8b8d3681c780b83
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-08-03 03:42:14 -0300

    refactor: move SUT creation to a factory method

--- src/main/presentation/components/private-route/private-route.spec.tsx
+++ src/main/presentation/components/private-route/private-route.spec.tsx
@@ -6,37 +6,46 @@ import { PrivateRoute } from './private-route'
 import { ConfigRoute } from '@/config/index'
 import { countQuantityRoute } from '@/utils/create-memory-history'
 import { render } from '@testing-library/react'
-import { createMemoryHistory } from 'history'
+import { createMemoryHistory, MemoryHistory } from 'history'
 
-const history = createMemoryHistory({
-  initialEntries: [ConfigRoute.fourDev.default.source.path] // Ponto de partida /
-})
+type SutTypes = {
+  history: MemoryHistory
+}
 
-// Adicionar um listener para atualizar a lista de rotas visitadas sempre que a localização do histórico mudar
-history.listen((location) => {
-  countQuantityRoute({
-    location
+const makeSut = (): SutTypes => {
+  const history = createMemoryHistory({
+    initialEntries: [ConfigRoute.fourDev.default.source.path] // Ponto de partida /
   })
-})
+
+  // Adicionar um listener para atualizar a lista de rotas visitadas sempre que a localização do histórico mudar
+  history.listen((location) => {
+    countQuantityRoute({
+      location
+    })
+  })
+
+  render(
+    <Router location={history.location} navigator={history}>
+      <Routes>
+        <Route
+          path={ConfigRoute.fourDev.default.source.path}
+          element={<PrivateRoute />}
+        >
+          <Route
+            path={ConfigRoute.fourDev.default.source.path}
+            element={<SurveyList />}
+          />
+        </Route>
+      </Routes>
+    </Router>
+  )
+
+  return { history }
+}
 
 describe('PrivateRoute', () => {
   test('Should redirect to /login if token is empty', () => {
-    render(
-      <Router location={history.location} navigator={history}>
-        <Routes>
-          <Route
-            path={ConfigRoute.fourDev.default.source.path}
-            element={<PrivateRoute />}
-          >
-            <Route
-              path={ConfigRoute.fourDev.default.source.path}
-              element={<SurveyList />}
-            />
-          </Route>
-        </Routes>
-      </Router>
-    )
-
+    const { history } = makeSut()
     expect(history.location.pathname).toBe(ConfigRoute.fourDev.login.path)
   })
 })
