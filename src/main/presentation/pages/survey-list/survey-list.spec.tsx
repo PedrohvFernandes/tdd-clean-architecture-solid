7a7d7ecd2079eff953816976001497c9bb6b1721
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-08-06 22:20:38 -0300

    refactor: move SUT cration to a factory method

--- src/main/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/main/presentation/pages/survey-list/survey-list.spec.tsx
@@ -2,9 +2,13 @@ import { SurveyList } from './survey-list'
 
 import { render, screen } from '@testing-library/react'
 
+const makeSut = (): void => {
+  render(<SurveyList />)
+}
+
 describe('SurveyList Component', () => {
   test('Should present 4 empty items on start', async () => {
-    render(<SurveyList />)
+    makeSut()
     const surveyList = screen.getByTestId('survey-list')
     // li>footer:empty retorna todos os elementos footer que estão vazios dentro de um li
     expect(surveyList.querySelectorAll('li>footer:empty').length).toBe(4)
