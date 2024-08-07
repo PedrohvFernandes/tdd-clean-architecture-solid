bdcd4510e1ad88e4b2066b2e1e690af5fa47a531
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-08-06 22:36:20 -0300

    feat: ensure SurveyList Component calls LoadSurveyList

--- src/main/presentation/pages/survey-list/survey-list.tsx
+++ src/main/presentation/pages/survey-list/survey-list.tsx
@@ -1,6 +1,19 @@
+import { useEffect } from 'react'
+
 import { SurveyItemEmpty } from './components'
 
-export function SurveyList() {
+import { LoadSurveyList } from '@/domain/usecases/load-survey-list'
+
+type Props = {
+  loadSurveyList: LoadSurveyList
+}
+
+export function SurveyList({ loadSurveyList }: Props) {
+  useEffect(() => {
+    ;(async function () {
+      loadSurveyList.loadAll()
+    })()
+  }, [loadSurveyList])
   return (
     <div className="flex flex-col self-center w-full max-w-[800px] flex-grow py-6 px-10 gap-10 bg-disabled-background">
       <h2 className="text-primary-DARK text-xl font-bold uppercase">
