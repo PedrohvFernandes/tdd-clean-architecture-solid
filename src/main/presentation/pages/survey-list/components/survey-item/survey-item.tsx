36c820ad276b61566c47bff725d13c7f20f095c5
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-08-07 18:03:31 -0300

    feat: ensure SurveyItem load icon

--- src/main/presentation/pages/survey-list/components/survey-item/survey-item.tsx
+++ src/main/presentation/pages/survey-list/components/survey-item/survey-item.tsx
@@ -12,21 +12,20 @@ export const SurveyItem = ({ survey }: SurveyItemProps) => {
   console.log(survey)
 
   const date = {
-    day: survey.date.getDate().toString().padStart(2, '0'),
+    day: survey.date.getDate().toString().padStart(2, '0'), // o padStart garante que sempre teremos 2 digitos, e caso não tenha ele preenche com 0. Ex: dia 2 -> 02, dia 12 -> 12
     month: survey.date
       .toLocaleString('pt-BR', { month: 'short' })
       .replace('.', ''),
     year: survey.date.getFullYear()
   }
 
+  const iconName = survey.didAnswer ? IconName.THUMBS_UP : IconName.THUMBS_DOWN
+
   return (
     <li className="flex flex-col justify-between  basis-[100%] smartphone-md:basis-[48%] h-64 bg-white rounded-lg shadow-shape">
       {/* <div className="flex justify-between flex-grow items-center bg-no-repeat bg-gradient-to-r to-primary-LIGHT from-primary-LIGHT  bg-size-58px"> */}
       <div className="flex justify-between flex-grow items-center bg-no-repeat bg-gradiente-to-right-primary-light  bg-size-58px rounded-lg relative">
-        <Icon
-          iconName={IconName.THUMBS_UP}
-          className="absolute -top-3 -right-3 p-3"
-        />
+        <Icon iconName={iconName} className="absolute -top-3 -right-3 p-3" />
 
         <time className="flex flex-col bg-primary-LIGHT text-white rounded-lg ml-6 justify-center w-16 h-28 items-center flex-shrink-0 gap-1 ">
           <span data-testid="day" className="text-5xl font-bold">
