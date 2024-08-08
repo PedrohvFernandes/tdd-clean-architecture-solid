46d4c039b5720277b02afccf676ca60195e03ed0
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-08-08 15:00:33 -0300

    test: ensure SurveyList render SurveyItems on success

--- src/main/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/main/presentation/pages/survey-list/survey-list.spec.tsx
@@ -1,14 +1,16 @@
 import { SurveyList } from './survey-list'
 
 import { SurveyModel } from '@/domain/models'
+import { mockSurveyListModel } from '@/domain/test'
 import { LoadSurveyList } from '@/domain/usecases/load-survey-list'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 
 export class LoadSurveyListSpy implements LoadSurveyList {
+  surveys = mockSurveyListModel()
   callsCount = 0
   async loadAll(): Promise<SurveyModel[]> {
     this.callsCount++
-    return []
+    return this.surveys
   }
 }
 
@@ -30,11 +32,24 @@ describe('SurveyList Component', () => {
     makeSut()
     const surveyList = screen.getByTestId('survey-list')
     // li>footer:empty retorna todos os elementos footer que estão vazios dentro de um li
-    expect(surveyList.querySelectorAll('li>footer:empty').length).toBe(4)
+    expect(surveyList.querySelectorAll('li>footer:empty')).toHaveLength(4)
+    // Aqui o teste só finaliza quando o surveyList for renderizado vazio, porque não esperamos a promise do useEffect
+    await waitFor(() => surveyList)
   })
 
   test('Should call LoadSurveyList', async () => {
     const { loadSurveyListSpy } = makeSut()
     expect(loadSurveyListSpy.callsCount).toBe(1)
+    // O teste so finaliza quando a função passada para o waitFor retornar verdadeiro, nesse caso caso na tela tenha um heading
+    await waitFor(() => screen.getByRole('heading'))
+  })
+
+  test('Should render SurveyItems on success', async () => {
+    makeSut()
+    const surveyList = screen.getByTestId('survey-list')
+    // Espera até que o surveyList tenha sido renderizado, esperando que ele tenha 10 li, depois de concluir a promise do useEffect, por isso o await no waitFor antes do expect, pois aguardamos a promise do useEffect
+    await waitFor(() => surveyList)
+
+    expect(surveyList.querySelectorAll('li')).toHaveLength(10)
   })
 })
