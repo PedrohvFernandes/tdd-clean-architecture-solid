c5f28eee27ddfb82aa792f1a7b7c2d026c9478d3
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-08-08 16:19:19 -0300

    feat: ensure SurveyList render error on failure

--- src/main/presentation/pages/survey-list/survey-list.tsx
+++ src/main/presentation/pages/survey-list/survey-list.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable multiline-ternary */
 import { useEffect, useState } from 'react'
 
+import { ButtonDefault } from '../../components/buttons'
 import { SurveyItem, SurveyItemEmpty } from './components'
 
 import { SurveyModel } from '@/domain/models'
@@ -10,32 +11,50 @@ type Props = {
   loadSurveyList: LoadSurveyList
 }
 
-export function SurveyList({ loadSurveyList }: Props) {
+export function SurveyList({ loadSurveyList }: Readonly<Props>) {
   const [state, setState] = useState({
-    surveys: [] as SurveyModel[]
+    surveys: [] as SurveyModel[],
+    error: ''
   })
 
   useEffect(() => {
-    loadSurveyList.loadAll().then((surveys) => setState({ surveys }))
-  }, [loadSurveyList])
+    loadSurveyList
+      .loadAll()
+      .then((surveys) => setState((old) => ({ ...old, surveys })))
+      .catch((error) => setState((old) => ({ ...old, error: error.message })))
+  }, [loadSurveyList, state])
 
   return (
     <div className="flex flex-col self-center w-full max-w-[800px] flex-grow py-6 px-10 gap-10 bg-disabled-background">
       <h2 className="text-primary-DARK text-xl font-bold uppercase">
         Enquetes
       </h2>
-      <ul
-        className="flex flex-wrap justify-between gap-6"
-        data-testid="survey-list"
-      >
-        {state.surveys.length ? (
-          state.surveys.map((survey) => (
-            <SurveyItem key={survey.id} survey={survey} />
-          ))
-        ) : (
-          <SurveyItemEmpty />
-        )}
-      </ul>
+
+      {state.error ? (
+        <div
+          className="text-red-500 bg-red-100 border border-red-400 px-4 py-3 rounded relative"
+          role="alert"
+        >
+          <strong className="font-bold">Erro!</strong>
+          <span className="block sm:inline" data-testid="error">
+            {state.error}
+          </span>
+          <ButtonDefault>Tentar novamente</ButtonDefault>
+        </div>
+      ) : (
+        <ul
+          className="flex flex-wrap justify-between gap-6"
+          data-testid="survey-list"
+        >
+          {state.surveys.length ? (
+            state.surveys.map((survey) => (
+              <SurveyItem key={survey.id} survey={survey} />
+            ))
+          ) : (
+            <SurveyItemEmpty />
+          )}
+        </ul>
+      )}
     </div>
   )
 }
