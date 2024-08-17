e942c1ee5672067771b55de06e52709fdd39c0d7
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-08-17 19:32:34 -0300

    feat: ensure AuthorizeHttpGetClientDecorator merge token to HttpGetClient

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
@@ -15,10 +15,20 @@ export class AuthorizeHttpGetClientDecorator implements HttpGetClient {
     const account = this.getStorage.get('account')
     if (account?.accessToken) {
       // Colocamos o token de acesso no header da requisição dentro de params. Dessa forma o token de acesso será enviado em todas as requisições que utilizarem o AuthorizeHttpGetClientDecorator. Para validar se o token de acesso está sendo enviado corretamente, se não tem alguem passando um token manualmente para o localStorage.
+      // Object.assign(params, {
+      //   headers: {
+      //     'x-access-token': account.accessToken
+      //   }
+      // })
+
+      // Aqui estamos fazendo a mesma coisa que o código acima, porém estamos utilizando o Object.assign para não sobrescrever o headers que já existem na requisição. Se ele já tiver headers, ele vai adicionar o token de acesso no header que já existe.
       Object.assign(params, {
-        headers: {
-          'x-access-token': account.accessToken
-        }
+        headers: Object.assign(
+          {
+            'x-access-token': account.accessToken
+          },
+          params.headers || {} // Se ele não tiver headers, ele vai passar um objeto vazio para não dar erro no Object.assign(
+        )
       })
     }
     // Com ou sem token de acesso, a requisição é feita normalmente. Com isso iremos validar se o token de acesso está sendo enviado corretamente.
