d88888df2ce5e7f188de7d3304655741ea9bc02f
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-08-23 18:37:58 -0300

    refactor: incorrect application path

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { App } from './app'
+import { App } from './App'
 import './index.css'
 
 // Na aula o professor concentra as pages tudo no main, irei deixar o main somente para os providers caso tenha. E no app para as pages ou routes
