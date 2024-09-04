1a4c35871c9cc1239c3b61a0691f7134c8ea2dcc
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-09-03 21:00:53 -0300

    chore: config travis ci

--- src/domain/errors/index.ts
+++ src/domain/errors/index.ts
@@ -7,6 +7,5 @@
 
 export * from './invalid-credentials-error'
 export * from './unexpected-error'
-export * from './invalid-save-access-token'
 export * from './email-in-use-error'
 export * from './access-denied-error'
